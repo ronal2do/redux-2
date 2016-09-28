9e40306824c1630c43b6a72dfd76cf94ac4fc5fb
Ronaldo Lima <2dlima@2d-Lima.local>
2016-09-28 01:01:24 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
