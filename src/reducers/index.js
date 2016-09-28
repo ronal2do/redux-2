3333d5e5c5ea58a2bcc23e856a62dc0f5dececc5
Ronaldo Lima <2dlima@2d-Lima.local>
2016-09-28 01:17:10 -0300

    dispatch and promisse ok

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './weather';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/weather.js
@@ -0,0 +1,9 @@
+export default function(state = null, action) {
+  console.log('Action received', action);
+  return state;
+}
+
+// export default function(state = null, action) {
+//   console.log('Action received', action);
+//   return state;
+// }
